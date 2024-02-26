ec0b5201d17311c3bb9f2050e6f1a0aeaa8a15f9
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2024-02-26 16:46:48 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 const App = () => {
 
   let x = "Hi.. User  Welcome to our first Project."
-
+  let array = ["user1", "user2", "user3", "user4", "user5"]
   return (
     <div>
       {x}
+      {array.map((user)=>{return <h1>{user}</h1>})}
     </div>
   )
 }
