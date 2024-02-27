8346ef479296daa124ed38766454c5e637a15fec
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2024-02-27 17:59:27 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import Banner from "./components/Banner"
+import Footer from "./components/Footer"
+import HowJsxWorks from "./components/HowJsxWorks"
 import Navbar from "./components/Navbar"
+import Sidebar from "./components/Sidebar"
 
 const App = () => {
+
+  let x = "Welcome to First React JS Project."
+
   return (
     <div>
       <Navbar/>
+      <Sidebar/>
+      <Footer/>
+      <Banner/>
+      <HowJsxWorks/>
+      {x}
     </div>
   )
 }
