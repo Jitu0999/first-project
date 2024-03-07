55ca609c33b20f9bdf5e4ce505bfb05e7f228530
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2024-03-07 22:26:55 +0530

    Update Todo.jsx

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,7 +2,8 @@ import'./CSS/Todo.css'
 const Todo = () => {
   return (
     <div className='todo'>
-      <div className='todo-header'></div>
+      <div className='todo-header'>To-Do List</div>
+      
       
     </div>
   )
