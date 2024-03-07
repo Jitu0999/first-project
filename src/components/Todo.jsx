bbc8eb331cf24f9728ef404bfca7b06fcdf63206
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2024-03-07 22:31:39 +0530

    Update Todo.jsx

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -4,7 +4,8 @@ const Todo = () => {
     <div className='todo'>
       <div className='todo-header'>To-Do List</div>
       <div className='tod-add'>
-        <input type='text' placeholder='Add Your Task'
+        <input type='text' placeholder='Add Your Task' className='todo-input' />
+        
       </div>
       
     </div>
